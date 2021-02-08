237fd3fd421a7ba5009385a59f2bff3e0899673b
muhammadaqilp <muhammadpratama1816@gmail.com>
2021-02-08 12:12:58 +0700

    menyelesaikan layout library

--- app/src/main/java/com/example/eperpusapp/MainActivity.java
+++ app/src/main/java/com/example/eperpusapp/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.eperpusapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.example.eperpusapp.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+//    private ActivityMainBinding binding;
+    BottomNavigationView bottomNavigationView;
+    NavController navController;
+    AppBarConfiguration appBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        binding = ActivityMainBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+
+        bottomNavigationView = findViewById(R.id.nav_view);
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+        appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_mybook, R.id.navigation_history
+        ).build();
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
