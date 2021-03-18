555a6fe59e6a2713c1c57a1d7e488e86627bdcf2
muhammadaqilp <muhammadpratama1816@gmail.com>
2021-03-18 11:41:19 +0700

    menambahkan ip untuk access data

--- app/src/main/java/com/example/eperpusapp/Network/ApiService.java
+++ app/src/main/java/com/example/eperpusapp/Network/ApiService.java
@@ -7,9 +7,10 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiService {
 
-//    private static String BASE_URL = "http://192.168.1.17:1298/"; //office
+    //    private static String BASE_URL = "http://192.168.1.17:1298/"; //office
 //    private static String BASE_URL = "http://172.24.144.1:5000/"; //localhost
-private static String BASE_URL = "http://192.168.100.22:5000/"; //localhost
+//private static String BASE_URL = "http://192.168.100.22:5000/"; //localhost
+    private static String BASE_URL = "http://192.168.1.29:5000/"; //localhost
     private static HttpLoggingInterceptor loggingInterceptor = null;
     private static Retrofit retrofit = null;
     private static OkHttpClient client = null;
